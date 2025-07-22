3d2491eb7c363720229843ab8871906292aa5d9f
Rita Leitao <ritaleitao4@gmail.com>
2025-07-22 15:42:45 +0100

    Add clear button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Stack, Typography, Button} from '@mui/material';
 import './data/datastore';
 import { ProductTable } from './components';
 import { getProducts, getProperties, getProperty } from './api';
@@ -19,11 +19,29 @@ function App() {
 		[selectedProperty, selectedOperatorId, filter]
 	);
 
+	const handleClearClick = () => {
+		setSelectedPropertyId('');
+		setSelectedOperatorId('');
+		setFilter('');
+	};
+
 	return (
 		<Container maxWidth="md">
 			<Box sx={{ my: 4 }}>
 				<Typography variant="h1">Condition Editor UI</Typography>
 				<Typography variant="h2">A Coding Exercise for UI Developers</Typography>
+				<Stack direction={{ xs: 'column', md: 'row' }} justifyContent="flex-start" gap={3} sx={{ mb: 2 }}>
+
+					<Button
+						variant="contained"
+						sx={{ flexGrow: { xs: 1, md: 0 }, marginLeft: { xs: 0, md: 'auto' } }}
+						onClick={handleClearClick}
+						disabled={selectedPropertyId === ''}
+						data-testid="clear-button"
+					>
+						Clear
+					</Button>
+				</Stack>
 				<ProductTable products={products} properties={properties} />
 			</Box>
 		</Container>
